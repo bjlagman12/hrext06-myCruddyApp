71ce022fd49d464afce1ae6233d86453b6434a1f
Brian Lagman <bjlagman@gmail.com>
2018-12-18 19:51:29 -0800

    inputted add expense buttons to function

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 $(document).ready(function(){
   console.log('jQuery loaded');
   var counter = 0
+
+  // need to access additional expenses
+ 
+
   // set name
   var myNameInStorage = localStorage.getItem('name');
   // set age
@@ -10,44 +14,55 @@ $(document).ready(function(){
 
   // write to local storage from input when button save clicked
   $('.btn-submit').on('click', function(){
+
     localStorage.setItem('name', $('.entry-name').val());
     localStorage.setItem('age', $('.entry-age').val());
-    localStorage.setItem('paycheck',$('.entry-paycheck').val())
-
-    var newExpenses = 'addexpenses' + counter
-    // console.log(counter)
-
-    // console.log(newExpenses)
-    localStorage.setItem(newExpenses,$('.add-expenses').val())
+    localStorage.setItem('paycheck',$('.entry-paycheck').val());
+    localStorage.setItem('saving',$('.entry-saving').val());
+    localStorage.setItem('investing',$('.entry-invest').val());
+    localStorage.setItem('rent',$('.entry-rent').val());
+    localStorage.setItem('bills',$('.entry-bills').val());
+    localStorage.setItem('food',$('.entry-food').val());
 
     // console.log($('.entry-name').val())
-
-    // display the value here
-    // for(var i = 0; i < localStorage.length; i++){
-
-
-
-    // }
-    $('.list-display-field').append('<div>' + myNameInStorage+ '</div>');
+    // $('.list-display-field').append('<div>' + myNameInStorage+ '</div>');
   
-
-
   });
 
   // add more inputs when button is pressed
   
   $('.btn-add').on('click',function(){
     // var counter = $('.')
-    
+    var entryAddOns = 'entry-add-expenses' + counter
+    var newExpenses = 'addexpenses' + counter;
+    var btnSubmit = 'btn-submit' + counter;
+    var btnDelete = 'btn-delete' + counter;
+
     counter++;
-    console.log(counter)
-    $('.all_text_entry').append('<input class="addexpenses" type="text" placeholder="Additional Expenses"><button class="btn-delete" type="button">delete</button><br>')
+    
+    // console.log(entryAddOns)
+    $('.all_text_entry').append('<input class="' + entryAddOns + '" type="text" placeholder="Additional Expenses"><button class="' + btnSubmit + '" type="button">submit</button><button class="' + btnDelete +'" type="button">delete</button>')
 
+      $('.' + btnSubmit).on('click',function(){
+        localStorage.setItem(newExpenses,$('.'+ entryAddOns).val())
+        // console.log(entryAddOns)
+      })
 
+      $('.'+ btnDelete).on('click',function(){
+
+        localStorage.removeItem(newExpenses)
+        $('.'+ entryAddOns).remove()
+        $('.'+ btnSubmit).remove()
+        $('.'+ btnDelete).remove()
+      
+      })
+
+    
   });
 
   // delete from local storage when delete button clicked
   $('.btn-delete').on('click', function(){
+
     counter--
     console.log(counter)
     // localStorage.removeItem('');
